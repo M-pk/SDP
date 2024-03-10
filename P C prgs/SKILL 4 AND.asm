23c1f73ac8bd1f02c782da79b36c6039197ae68c
M-pk <mpavankumar877@gmail.com>
2024-03-10 06:04:06 +0530

    first commit

---
+++ P C prgs/SKILL 4 AND.asm
@@ -0,0 +1,14 @@
+
+org 100h
+
+
+MOV AL,01H
+MOV BL,11H
+OR   AL,BL
+MOV [5000H],AL                                                                                                                             
+
+ret
+
+
+
+
